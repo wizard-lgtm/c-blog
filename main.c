a3b8298b3380bc0cbcc26bee0852421bbb110df1
wizard-lgtm <rippiten23@gmail.com>
2024-10-23 10:02:54 -0700

    db added

--- main.c
+++ main.c
@@ -1,16 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "markdown_operations.h"
+#include "dotenv.h"
+#include "db_operations.h"
+#include <mongoc/mongoc.h>
+
 int main() {
-    char* filename = "./file.md";
-    char* html = convert_md_file_to_html(filename);
-    if (html) {
-        printf("Converted HTML:\n%s\n", html);
-        free(html);
-    } else {
+    env_load(".", false);
+    mongoc_client_t* client = connect_db();
+    if(client == NULL){
+        perror("Some error happened while connecting to db");
         return 1;
     }
 
+    char* filename = "./file.md";
+    char* html = convert_md_file_to_html(filename);
+    if (!html) {
+        return 1;
+    }
+    printf("Converted HTML:\n%s\n", html);
 
+    free(html);
+    cleanup_db(client);
     return 0;
 }
